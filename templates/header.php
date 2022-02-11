544a9432b5558ea36f4435a7924bff0a437c8afe
Gabriel Marques <bielmarques922@gmail.com>
2022-02-11 16:52:58 -0300

    Ajustando as Paginas

--- templates/header.php
+++ templates/header.php
@@ -2,24 +2,8 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<?php if(is_home() && !is_front_page()) { ?>
-		<title><?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo', get_option('page_for_posts')); ?></title>
-		<?php } else { ?>
-		<title><?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?></title>
-		<?php } ?>
-		<meta name="description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>">
-
-		<meta property="og:type" content="website"/>
-		<meta property="og:title" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
-		<meta property="og:description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>"/>
-		<meta property="og:url" content="<?php bloginfo('url'); ?>"/>
-		<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/og-image.png"/>
-
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<!-- Inicio Wordpress Header -->
-		<?php wp_head(); ?>
-		<!-- Final Wordpress Header -->	
+		<title>Konnors</title>
 	</head>
 	<body>
 
