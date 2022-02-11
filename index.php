d9c974bc6ff583beb9999f251ca5f9d372a5488f
Gabriel Marques <bielmarques922@gmail.com>
2022-02-11 16:34:04 -0300

    Implementando Sass

--- index.php
+++ index.php
@@ -1,14 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <link rel="stylesheet" href="style.css">
-  <title>Konnors</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="shortcut icon" href="/img/Logo.png">
+        <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Racing+Sans+One&display=swap"
+            rel="stylesheet">
+        <title>Konnors</title>
+    </head>
 <body>
-  
+    <header>
+        <div class="container">
+            <div class="header">
+                <div class="logo">
+                    <h2 class="logo"><a href="index.html">Konnors</a></h2>
+                </div>
+                <div class="nav">
+                    <nav id="nav">
+                        <button id="btn-mobile" aria-label="Abrir Menu" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
+                            <span id="hamburger"></span>
+                        </button>
+                        <ul role="menu" class="menu" id="menu">
+                            <li><a href="">Serviços</a></li>
+                            <li><a href="">Templates</a></li>
+                            <li><a href="">Planos & Preços</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section class="banner"> 
+            <div class="container">
+                <div class="banner__content">
+                    <div class="banner__content__text">
+                        <h1>Tiramos a sua ideia do papel. Do design ao site no ar.</h1>
+                        <p>Do design ao site no ar.</p>
+                        <a href="#">NOSSOS TEMPLATES </a>
+                    </div>
+                    <div class="banner__content__img">
+                        <img src="img/pexels-picjumbocom-210661 1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <script src="js/menu.js"></script>
 </body>
 </html>
