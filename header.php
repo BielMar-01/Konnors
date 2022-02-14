9bcb23aed8c393c85ec1d71d29a5bedce0b53f36
Matheus Felix <matheuzfelixi2@gmail.com>
2022-02-14 16:00:30 -0300

    tirando o header do partials e adicao de campos personalizados e menus integrados com o wordpress

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js <?php echo ICL_LANGUAGE_CODE ?>">
 <!-- script_path: <?php echo basename(__FILE__, ''); ?> -->
 <head>
-    <title><?php wp_title('-', true, 'right'); ?></title>
+    <title><?php bloginfo('name')?> <?php wp_title() ?></title>
     <meta name="description" content="<?php bloginfo('description'); ?>">
 
     <meta charset="UTF-8">
@@ -16,4 +16,24 @@
 </head>
 
 <body id="lang-<?php echo ICL_LANGUAGE_CODE ?>" <?php  body_class(); ?>>
-<?php get_template_part('partials/header', 'global'); ?>
+    <header class="header">
+        <div class="container">
+            <div class="header__top">
+                <h2 class="logo"><a href="<?php the_field('logo_link', 'option'); ?>">Konnors</a></h2>
+                <?php
+                        $args = array(
+                            'menu' => 'Primary Menu',
+                            'theme_location' => 'primary_menu',
+                            'container' => false
+                        );
+                        wp_nav_menu( $args );
+                    ?>
+            </div>
+            <div class="text__banner">
+                <p><?php the_field('banner_text'); ?></p>
+            </div>
+            <div class="arrow">
+                <img src="<?php echo get_template_directory_uri()?>img/Arrow.png" alt="Arrow">
+            </div>
+        </div>
+    </header>
