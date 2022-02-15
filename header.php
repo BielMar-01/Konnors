0058a143a32a4d0b7afd6176f6fc5c962cf51df8
Matheus Felix <matheuzfelixi2@gmail.com>
2022-02-15 11:30:28 -0300

    link de estilizacao

--- header.php
+++ header.php
@@ -2,18 +2,18 @@
 <html <?php language_attributes(); ?> class="no-js <?php echo ICL_LANGUAGE_CODE ?>">
 <!-- script_path: <?php echo basename(__FILE__, ''); ?> -->
 <head>
-    <?php wp_head(); ?>
     
     <title><?php bloginfo('name')?> <?php wp_title() ?></title>
     <meta name="description" content="<?php bloginfo('description'); ?>">
-
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Language" content="<?php echo ICL_LANGUAGE_CODE ?>">
-
+    
     <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/img/favicon.png" />
-
+    
+    <?php wp_head(); ?>
 </head>
 
 <body id="lang-<?php echo ICL_LANGUAGE_CODE ?>" <?php  body_class(); ?>>
